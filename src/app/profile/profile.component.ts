e0de3747773464702761cbec7686b7d82120b94a
MadhavPolawala-dds <madhavpolawala.dds@gmail.com>
2024-09-19 12:42:21 +0530

    logout

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { UserService } from '../services/user.service';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css',
 })
 export class ProfileComponent {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private router: Router) {}
   ngOnInit() {
     this.userService.profile();
+  }
+  onLogout() {
+    this.router.navigate(['/']);
+    localStorage.removeItem('token');
   }
 }
